91119ce8d5d098cf887759644f52823c2c1d9bd8
robalar <rbthales@gmail.com>
2016-01-24 20:22:43 +0000

    Added recursive subs

--- solver/core/subs.py
+++ solver/core/subs.py
@@ -1,12 +1,20 @@
+from .expr import Expression
+
+
 def subs(expression, old_term, new_term):
 
-    #FIXME: Add type checking
-    #TODO: Add multiple subs per call
+    # FIXME: Add type checking
+    # TODO: Add multiple subs per call
+
+    if not isinstance(expression, Expression):
+        return expression
 
     final_expression = expression
 
     for i, sub_expression in enumerate(final_expression):
         if sub_expression == old_term:
             final_expression.args[i] = new_term
+
+        subs(sub_expression, old_term, new_term)
 
     return final_expression
