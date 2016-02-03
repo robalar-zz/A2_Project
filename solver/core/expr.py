e5629a3d58048768057edbc8ad66c6287475ffd2
robalar <rbthales@gmail.com>
2016-02-03 18:59:43 +0000

    reworked and simplified subs

--- solver/core/expr.py
+++ solver/core/expr.py
@@ -5,9 +5,25 @@ class Expression(Base):
 
     def __new__(cls, *args):
         obj = Base.__new__(cls)
-        obj.args = list(args)
+        obj.args = list(args)  # Makes expression mutable, change?
 
         return obj
+
+    def replace(self, old, new):
+        self.args = [new if x == old else x for x in self.args]
+        return self
+
+    def __contains__(self, item):
+
+        if item.__class__ == self.__class__ and _sublist(self.args, item.args):
+            return True
+
+        for sub_expr in self:
+            if sub_expr == item:
+                return True
+
+            if isinstance(sub_expr, Expression) and item in sub_expr:
+                return True
 
     def __repr__(self):
         return '{}({})'.format(self.__class__.__name__, str(self.args)[1:-1])
@@ -15,6 +31,27 @@ class Expression(Base):
     def __iter__(self):
         return iter(self.args)
 
+    def __eq__(self, other):
+        if self.__class__ == other.__class__ and self.args == other.args:
+            return True
+        else:
+            return False
 
-def is_redex(obj):
-    return hasattr(obj, 'args')
+
+def subexpressions(expression, types=Expression):
+    return [x for x in expression.args if isinstance(x, types)]
+
+
+def _sublist(list, pattern):
+    return [x for x in list if x in set(pattern)]
+
+
+def postorder(node):
+
+    if isinstance(node, Expression):
+        for arg in node:
+            for sub in postorder(arg):
+                yield sub
+
+    yield node
+
--- solver/core/subs.py
+++ solver/core/subs.py
@@ -4,18 +4,19 @@ import copy
 
 def subs(expression, old_term, new_term):
 
-    # FIXME: Add type checking
+    # TODO: Add rearrangement of old term
     # TODO: Add multiple subs per call
 
     final_expression = copy.deepcopy(expression)
+
+    if old_term == new_term:
+        return final_expression
 
     if final_expression == old_term:
         final_expression = new_term
 
     for node in postorder(final_expression):
         if isinstance(node, Expression):
-            for i, sub_expr in enumerate(node.args):
-                if sub_expr == old_term:
-                    node.args[i] = new_term
+            node.replace(old_term, new_term)
 
     return final_expression
