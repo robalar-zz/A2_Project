a18ef2f5bf624535a93182f5f2f0ef14507c8b1d
robalar <rbthales@gmail.com>
2016-02-13 11:51:20 +0000

    streamlined classes due to builtin magic

--- solver/core/numbers.py
+++ solver/core/numbers.py
@@ -27,6 +27,42 @@ class Number(Atom):
     def __deepcopy__(self, memo):
         return self
 
+    def __eq__(self, other):
+        if isinstance(other, Number):
+            return self.value == other.value
+        else:
+            return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __lt__(self, other):
+        if isinstance(other, Number):
+            return self.value < other.value
+        else:
+            return False
+
+    def __le__(self, other):
+        if isinstance(other, Number):
+            return self.value <= other.value
+        else:
+            return False
+
+    def __gt__(self, other):
+        if isinstance(other, Number):
+            return self.value > other.value
+        else:
+            return False
+
+    def __ge__(self, other):
+        if isinstance(other, Number):
+            return self.value >= other.value
+        else:
+            return False
+
+    def __abs__(self):
+        return abs(Number(self.value))
+
 
 class Undefined(object):
     """ For calculations where the result is known i.e. 0/0, 0^0, oo/oo
@@ -56,66 +92,6 @@ class Integer(Number):
         
         self.value = value
 
-    """def __add__(self, other):
-        if isinstance(other, Integer):
-            return Integer(self.value + other.value)
-        else:
-            return Atom.__add__(self, other)
-
-    def __iadd__(self, other):
-        return self + other
-
-    def __mul__(self, other):
-        if isinstance(other, Integer):
-            return Integer(self.value * other.value)
-        else:
-            return Base.__mul__(self, other)
-
-    def __rmul__(self, other):
-        return self.__mul__(other)
-
-    def __pow__(self, power, modulo=None):
-
-        if isinstance(power, Integer):
-            if power.value == 0 and self.value == 0:
-                return Undefined()
-
-            return Integer(self.value ** power.value)
-        else:
-            return Atom.__pow__(self, power)"""
-
-    def __eq__(self, other):
-
-        if isinstance(other, Integer):
-            return self.value == other.value
-        else:
-            return False
-
-    def __ne__(self, other):
-        return not self.__eq__(other)
-
-    def __lt__(self, other):
-        if isinstance(other, Integer):
-            return self.value < other.value
-
-    def __le__(self, other):
-        if isinstance(other, Integer):
-            return self.value <= other.value
-
-    def __gt__(self, other):
-        if isinstance(other, Integer):
-            return self.value > other.value
-
-    def __ge__(self, other):
-        if isinstance(other, Integer):
-            return self.value >= other.value
-
-
-
-    def __abs__(self):
-        return Number(abs(self.value))
-
-
 class Rational(Number):
     """ Purely a wrapper for the builtin fraction class
     """
@@ -129,22 +105,6 @@ class Rational(Number):
 
     def __str__(self):
         return self.value.__str__()
-    """
-    def __add__(self, other):
-        if isinstance(other, Rational):
-            return Fraction.__add__(self.fract, other.fract)
-
-    def __radd__(self, other):
-        if isinstance(other, Rational):
-            return Fraction.__add__(self.fract, other.fract)
-
-    def __mul__(self, other):
-        if isinstance(other, Rational):
-            return Fraction.__mul__(self._fract, other._fract)
-
-    def __rmul__(self, other):
-        if isinstance(other, Rational):
-            return Fraction.__mul__(self.fract, other.fract)"""
 
 
 def sum(list):
