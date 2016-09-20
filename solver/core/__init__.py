5b821a7e8354f286af237c511667ecff8f7c7932
robalar <rbthales@gmail.com>
2016-09-20 16:04:13 +0100

    formatting and eq is avalible on import

--- solver/core/__init__.py
+++ solver/core/__init__.py
@@ -1,7 +1,7 @@
 from .function import Function
 from .matix import Matrix, zeros, linear_space
 from .numbers import Number, Fraction, Integer, Rational, Undefined, ReservedSymbol
-from .operations import Add, Mul, Pow
+from .operations import Add, Mul, Pow, Eq
 from .parser import parse
 from .subs import substitute
 from .symbol import Symbol
